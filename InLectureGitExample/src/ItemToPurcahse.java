85701d960f99a47603fc4396c739c028d36aba4c
Jintong Wu <jintongw@seas.upenn.edu>
2020-10-15 13:45:38 -0400

    Added constructor

--- InLectureGitExample/src/ItemToPurcahse.java
+++ InLectureGitExample/src/ItemToPurcahse.java
@@ -3,4 +3,10 @@ public class ItemToPurcahse {
 	String itemName;
 	int itemPrice;
 	int itemQuality;
+	
+	ItemToPurcahse(String name, int price, int quality){
+		this.itemName = name;
+		this.itemPrice = price;
+		this.itemQuality = quality;
+	}
 }
